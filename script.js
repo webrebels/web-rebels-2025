5bea13136737b2bacba02e5e9fac76c05f5dad81
elisejakob <hei@elisejakob.no>
2020-02-02 19:50:34 +0100

    add close button to newsletter component

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// hide/show mobile menu
 function toggleMenu() {
   document.getElementById("site-nav").classList.toggle('open');
 }
 document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
 
 
+
+// hide newsletter component
+function hideNewsletter() {
+  document.getElementById("newsletter").classList.add('closed');
+}
+document.getElementById("close-newsletter").addEventListener("click", hideNewsletter);
+
+
+
+// switch between light/dark theme
 var lightTheme;
 
 if (typeof localStorage.lightTheme !== "undefined") {
