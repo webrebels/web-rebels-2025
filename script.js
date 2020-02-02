4f320b392ec5159a26c3fda7787277e9ecd320a7
elisejakob <hei@elisejakob.no>
2020-02-02 18:39:05 +0100

    add theme switch 🌗

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-  document.body.classList.add('light-theme');
+var lightTheme;
+
+if (typeof localStorage.lightTheme !== "undefined") {
+  lightTheme = JSON.parse(localStorage.lightTheme);
+} else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+  lightTheme = true
+} else {
+  lightTheme = false
 }
+
+function displayTheme() {
+  if (lightTheme) {
+    document.body.classList.add('light-theme');
+  } else {
+    document.body.classList.remove('light-theme');
+  }
+}
+
+function changeTheme() {
+  lightTheme = !lightTheme;
+  localStorage.lightTheme = lightTheme;
+  displayTheme();
+}
+
+document.getElementById("moon").addEventListener("click", changeTheme);
+
+displayTheme();
