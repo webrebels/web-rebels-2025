f80445a1627e56adb32675462762e8cd26c33c63
Anders Olsen Sandvik <github@mlsv.no>
2024-05-22 21:32:20 +0200

    Remove close button and text for newsletter

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 // hide/show mobile menu
 function toggleMenu() {
-  document.getElementById("site-nav").classList.toggle('open');
+  document.getElementById("site-nav").classList.toggle("open");
 }
 document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
 
-
-
 // hide newsletter component
-function hideNewsletter() {
-  document.getElementById("newsletter").classList.add('closed');
-}
-document.getElementById("close-newsletter").addEventListener("click", hideNewsletter);
-
-
+// function hideNewsletter() {
+//  document.getElementById("newsletter").classList.add('closed');
+// }
+// document.getElementById("close-newsletter").addEventListener("click", hideNewsletter);
 
 // switch between light/dark theme
 var lightTheme;
 
 if (typeof localStorage.lightTheme !== "undefined") {
   lightTheme = JSON.parse(localStorage.lightTheme);
-} else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-  lightTheme = true
+} else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+  lightTheme = true;
 } else {
-  lightTheme = false
+  lightTheme = false;
 }
 
 function displayTheme() {
   if (lightTheme) {
-    document.body.classList.add('light-theme');
+    document.body.classList.add("light-theme");
   } else {
-    document.body.classList.remove('light-theme');
+    document.body.classList.remove("light-theme");
   }
 }
 
